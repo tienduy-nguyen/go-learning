64e170d1e232d54f1e9a26ff23ce823647a57791
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-08-22 10:08:43 +0200

    Short hand for declaration string

--- cards-project/main.go
+++ cards-project/main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
+	// var card string = "Ace of Spades"
+	// := using only when declare an variable
+	card := newCard()
+
+	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
