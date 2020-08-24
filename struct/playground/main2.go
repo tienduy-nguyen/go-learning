a5d27c0f13ab58c7a60be2e930bafd912e9cf5a2
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-08-25 00:10:34 +0200

    Using pointer

---
+++ struct/playground/main2.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+	var alex person
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+
+	fmt.Println(alex)
+	fmt.Printf("%+v", alex)
+
+}
