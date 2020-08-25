8c5a9c98d0e602ff1fa3b3a6cab4b3fa6529908f
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-08-25 14:22:41 +0200

    Map in go

--- map/main.go
+++ map/main.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
+	colors := map[string](string){
 		"red":   "#ff0000",
-		"green": "#4bf748",
+		"white": "#fff",
+		"black": "#000",
+		"green": "#4bf745",
 	}
-	fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
---
+++ map/playground/main1.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf748",
+	}
+	fmt.Println(colors)
+}
